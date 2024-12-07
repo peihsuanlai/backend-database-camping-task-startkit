32f2dc3d4b8afff0b8e4d90a40103d9e93031f4e
peihsuan <june625678@gmail.com>
2024-12-07 10:59:18 +0800

    修正錯誤

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -171,7 +171,7 @@
         -- 3. 狀態`status` 設定為即將授課
     INSERT INTO "COURSE_BOOKING" (user_id, course_id, booking_at, status) VALUES
     (
-        (SELECT id FROM "USER" WHERE e-mail = 'wXlTq@hexschooltest.io'),
+        (SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
         (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io')),
         '2024-11-24 16:00:00',
         '即將授課'
